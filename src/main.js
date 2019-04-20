9a20e7906fb400090aa5c013023484d05618d025
hadv <dvietha@gmail.com>
2019-04-20 11:16:25 +0700

    using router with history mode

--- src/main.js
+++ src/main.js
@@ -1,26 +1,17 @@
 import Vue from 'vue'
-import VueRouter from 'vue-router'
 import BootstrapVue from 'bootstrap-vue'
 import App from './App.vue'
-import Home from './pages/Home.vue'
-import SignIn from './pages/SignIn.vue'
+import router from './router'
+import store from './store'
+
 import 'bootstrap/dist/css/bootstrap.css'
 import 'bootstrap-vue/dist/bootstrap-vue.css'
 
-Vue.use(VueRouter)
 Vue.use(BootstrapVue)
 Vue.config.productionTip = false
 
-const routes = [
-  { path: '/', component: Home },
-  { path: '/sign-in', component: SignIn }
-]
-
-const router = new VueRouter({
-  routes
-})
-
 new Vue({
   router,
+  store,
   render: h => h(App)
 }).$mount('#app')
